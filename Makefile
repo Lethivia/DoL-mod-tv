281b354853b5fba47737a364d920e8a47539abe1
Lethivia <lanyakernel@gmail.com>
2025-05-31 11:09:32 +0800

    fix

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
+VER=$(shell cat boot.json | grep version | cut -d'"' -f4 | head -1)
+MOD=$(shell basename ${PWD})
+
 all:
-	VER=$(cat boot.json | grep version | cut -d'"' -f4 | head -1);MOD=$(basename $PWD);zip -r ../DoL-$MOD-$VER.zip . -x'.git/*' -xMakefile; cp ../DoL-$MOD-$VER.zip /tmp/
+	zip -r ../DoL-${MOD}-${VER}.zip . -x'.git/*' -xMakefile; cp ../DoL-${MOD}-${VER}.zip /tmp/
--- game/03-JavaScript/tvMenu.js
+++ game/03-JavaScript/tvMenu.js
@@ -2,7 +2,7 @@ setup.tvmodav = {
 	"IPX-89": {
 		name: "超绝XXX",
 		desc: "讲述了一个xxx的故事",
-		img: "img/av/ipx89.png",
+		img: "img/av/1.png",
 		watch_passage: [
 			"第一幕，画面中一个xxx。。。",
 			"第二幕，xxx与xxx。。",
@@ -11,11 +11,12 @@ setup.tvmodav = {
 		stat_up: "<<gawareness>><<awareness 5>><<garousal>><<arousal 500>>",
 		passtime: 30,
 		cost:500,
+		type:"口交",
 	},
 	"HSODA-055":{
 		name: "XXXXXX",
 		desc: "xxx和xx的故事",
-		img: "img/av/ipx89.png",
+		img: "img/av/1.png",
 		watch_passage: [
 			"第一幕，画面中一个xxx。。。",
 			"第二幕，xxx与xxx。。",
@@ -24,5 +25,6 @@ setup.tvmodav = {
 		stat_up: "<<gawareness>><<awareness 5>><<garousal>><<arousal 500>>",
 		passtime: 30,
 		cost:800,
+		type:"SM",
 	}
 }
