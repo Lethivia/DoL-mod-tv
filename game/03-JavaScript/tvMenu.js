a18c2ebbd7286fe0262f8bf89438c2d974cce0c5
Lethivia <lanyakernel@gmail.com>
2025-05-31 10:10:13 +0800

    up

---
+++ game/03-JavaScript/tvMenu.js
@@ -0,0 +1,28 @@
+setup.tvmodav = {
+	"IPX-89": {
+		name: "超绝XXX",
+		desc: "讲述了一个xxx的故事",
+		img: "img/av/ipx89.png",
+		watch_passage: [
+			"第一幕，画面中一个xxx。。。",
+			"第二幕，xxx与xxx。。",
+			"最终，xxxxx"
+		],
+		stat_up: "<<gawareness>><<awareness 5>><<garousal>><<arousal 500>>",
+		passtime: 30,
+		cost:500,
+	},
+	"HSODA-055":{
+		name: "XXXXXX",
+		desc: "xxx和xx的故事",
+		img: "img/av/ipx89.png",
+		watch_passage: [
+			"第一幕，画面中一个xxx。。。",
+			"第二幕，xxx与xxx。。",
+			"最终，xxxxx"
+		],
+		stat_up: "<<gawareness>><<awareness 5>><<garousal>><<arousal 500>>",
+		passtime: 30,
+		cost:800,
+	}
+}
