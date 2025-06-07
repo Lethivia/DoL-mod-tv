bd2347f62a0b98eba210de55f129beaa14443ccf
Lethivia <lanyakernel@gmail.com>
2025-06-07 23:42:38 +0800

    up

--- game/tvMenu.js
+++ game/tvMenu.js
@@ -1,24 +1,10 @@
 setup.tvmodav = {
-	"IPX-89": {
-		name: "超绝XXX",
-		desc: "讲述了一个xxx的故事",
-		img: "img/av/1.png",
-		content: [
-			"一个男人正在亲吻一个女人。",
-			"女人呻吟着。",
-			"男人与女人一起高潮，女人满足的笑着"
-		],
-		stat_up: "<<gawareness>><<awareness 5>><<garousal>><<arousal 500>>",
-		passtime: 30,
-		cost:500,
-		type:"口交",
-	},
 	"HSODA-055":{
 		name: "XXXXXX",
 		desc: "苍#空和L的故事",
 		img: "img/av/3.png",
 		content: {
-            master: {
+            main: {
                 msg: [
 `
 剧情的场景在一个巷子里，男主名字叫<<avnpc 0>>，女主名字叫<<avnpc 1>>
